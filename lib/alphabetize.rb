b16b178fcce19f40e41d8a0171cfb48cb4b56c56
c0nniewang <connie.wang303@gmail.com>
2017-10-18 20:49:52 -0400

    Done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
 def alphabetize(arr)
-  # code here
+alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+alphahash = Hash.new(0)
+alphabet.split("").each.with_index {|ch, i| alphahash[ch] = i}
+
+  arr.sort_by do |string|
+    string.split("").map do |ch|
+      alphahash[ch]
+    end
+  end
 end
